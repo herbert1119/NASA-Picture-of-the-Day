a0fe7c69bb18ee483709028119136159e557b550
bhanbbm <bhan@bbmtek.com>
2019-02-09 08:08:37 -0500

    Initial Commit

---
+++ NASA Picture of the Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASA Picture of the Day
+//
+//  Created by Bo Han on 2/9/19.
+//  Copyright © 2019 Bo Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NASA Picture of the DayTests/NASA_Picture_of_the_DayTests.swift
@@ -0,0 +1,34 @@
+//
+//  NASA_Picture_of_the_DayTests.swift
+//  NASA Picture of the DayTests
+//
+//  Created by Bo Han on 2/9/19.
+//  Copyright © 2019 Bo Han. All rights reserved.
+//
+
+import XCTest
+@testable import NASA_Picture_of_the_Day
+
+class NASA_Picture_of_the_DayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
